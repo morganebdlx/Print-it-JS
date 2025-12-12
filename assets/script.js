78a0583b76016ee20ebf16ae9b3acc1d9c535f9c
Morgane Bourdillon <mlacroix8@gmail.com>
2025-12-12 15:54:15 +0100

    arrows et dots

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,45 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+
+let arrowRight = document.querySelector('.arrow_right');
+let arrowLeft = document.querySelector('.arrow_left');
+let bannerImage = document.querySelector('#banner img');
+let bannerText = document.querySelector('#banner p');
+let bannerDots = document.querySelector('.dots');
+
+
+// Dots section : pour chaque slide on crée et on ajoute une
+// dot dans le HTML dans la div .dots
+
+slides.forEach ((slide, index) => {
+  let dot = document.createElement('div');
+  dot.classList.add('dot');
+  if (index === 0) {
+    dot.classList.add('dot_selected');
+  }
+  bannerDots.appendChild(dot);
+});
+
+
+
+// Activation des boutons au clic : on écoute les clicls sur arrowRight et arrowLeft,
+// au clic on prend la focntion pour changer de +1 ou -1 slide
+
+arrowRight.addEventListener('click', function() {
+  changeSlide(-1);
+  console.log('clic droite');
+});
+
+arrowLeft.addEventListener('click', function() {
+  changeSlide(1);
+  console.log('clic gauche');
+});
+
+
+
+//Changement de slide : on récupère les images dans la constante slides, elle change quand
+// on clique sur une des flèches : on utilise la clé "image" pour changer l'image
+// et la clé "tagLine" pour changer le texte
