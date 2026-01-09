eb2be3fd3dd870e7ccb6bf262a284ddb018e0b75
Morgane Bourdillon <mlacroix8@gmail.com>
2026-01-09 14:09:25 +0100

    ok

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,2 @@
 # Print-It WebSite v.1
+# Print-it-JS
--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,62 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+
+let arrowRight = document.querySelector('.arrow_right');
+let arrowLeft = document.querySelector('.arrow_left');
+let bannerImage = document.getElementById('banner-img');
+let bannerText = document.querySelector('#banner p');
+let bannerDots = document.querySelector('.dots');
+
+let currentSlide = 0;
+
+
+// Dots section
+
+slides.forEach ((slide, index) => {
+  let dot = document.createElement('div');
+  dot.classList.add('dot');
+  if (index === 0) {
+    dot.classList.add('dot_selected');
+  }
+  bannerDots.appendChild(dot);
+});
+
+
+
+// Activation des boutons au clic
+
+arrowRight.addEventListener('click', function() {
+  changeSlide(1);
+});
+
+arrowLeft.addEventListener('click', function() {
+  changeSlide(-1);
+});
+
+
+// Fontion de changement de slide
+
+function changeSlide(direction) {
+
+  let bannerDotsList = document.querySelectorAll('.dot');
+
+  bannerDotsList[currentSlide].classList.remove('dot_selected');
+
+  currentSlide += direction;
+
+  if (currentSlide < 0) {
+    currentSlide = slides.length -1;
+  } else if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+
+  bannerDotsList[currentSlide].classList.add('dot_selected');
+
+  bannerImage.src = `assets/images/slideshow/${slides[currentSlide].image}`;
+  bannerText.innerHTML = slides[currentSlide].tagLine;
+
+}
+
